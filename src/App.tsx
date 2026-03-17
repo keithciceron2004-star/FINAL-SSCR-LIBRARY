60231a3d9c1933f74f7c279132f315b3e953340c
keithciceron2004-star <keithciceron2004@gmail.com>
2026-03-17 16:13:11 +0000

    Fix GitHub Pages routing by setting VITE_BASE_URL and BrowserRouter basename

--- src/App.tsx
+++ src/App.tsx
@@ -10,21 +10,25 @@ import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/borrower" element={<BorrowerDashboard />} />
-          <Route path="/librarian" element={<LibrarianDashboard />} />
-          <Route path="/supervisor" element={<SupervisorDashboard />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+const App = () => {
+  const baseName = import.meta.env.BASE_URL || "/";
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Sonner />
+        <BrowserRouter basename={baseName}>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/borrower" element={<BorrowerDashboard />} />
+            <Route path="/librarian" element={<LibrarianDashboard />} />
+            <Route path="/supervisor" element={<SupervisorDashboard />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
